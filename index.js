42fbfadf9441f14f2f03c07d23ac4c19079f5663
Nicolas Martiné <nicolas57415@gmail.com>
2019-12-05 17:52:34 +0100

    Preloader added

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+//create function that removes the preloader
+
+window.addEventListener("load", () => {
+  const preload = document.querySelector(".preload");
+  const body = document.body;
+  preload.classList.add("preload-finish");
+  body.style.overflow = "";
+});
+
 console.log("Henlo");
