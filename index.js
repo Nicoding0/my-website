85ec3840dabeb4c285a21f753fb0f9ca34740019
Nicolas Martiné <nicolas57415@gmail.com>
2020-01-13 17:07:33 +0100

    fixed preload & added favicon

--- index.js
+++ index.js
@@ -3,8 +3,9 @@
 window.addEventListener("load", () => {
   const preload = document.querySelector(".preload");
   const body = document.body;
+  preload.classList.remove("preload");
   preload.classList.add("preload-finish");
   body.style.overflow = "";
 });
 
-console.log("Henlo");
+console.log("Loaded");
